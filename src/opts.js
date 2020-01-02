89f08d86281e916f0712e074a3c96feb411ea74d
Billy <billybofh@gmail.com>
2020-01-02 14:31:24 +0000

    Updated default ttl to something sensible

--- src/opts.js
+++ src/opts.js
@@ -6,7 +6,7 @@ program
   .option('--mongo <connection-string>', 'connection string for mongodb', process.env.LITELOG_MONGO ? process.env.LITELOG_MONGO : 'mongodb://localhost:27017/litelogs')
   .option('--port <port-number>', 'port number to listen on', process.env.LITELOG_PORT ? process.env.LITELOG_PORT : 12201)
   .option('--ip <ip-address>', 'IP address to bind to', process.env.LITELOG_IP ? process.env.LITELOG_IP : '0.0.0.0')
-  .option('--ttl <hours>', 'number of hours to retain logs', process.env.LITELOG_TTL ? process.env.LITELOG_TTL : 0.1)
+  .option('--ttl <hours>', 'number of hours to retain logs', process.env.LITELOG_TTL ? process.env.LITELOG_TTL : 24 * 7 * 4)
   .option('--user <username>', 'username for mongodb', process.env.LITELOG_USER ? process.env.LITELOG_USER : '')
   .option('--password <password>', 'password for mongodb', process.env.LITELOG_PASSWORD ? process.env.LITELOG_PASSWORD : '')
   .option('--forward <gelf-server>', 'forward messages to this server', process.env.LITELOG_FORWARD ? process.env.LITELOG_FORWARD : '')
